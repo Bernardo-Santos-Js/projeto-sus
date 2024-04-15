eb905d8d3c0b45c490275483216ca7cf2ddb408f
Jéssica <jehdg05@gmail.com>
2024-04-15 10:11:17 -0300

    atualizando as informações do formulario e atuando no arquivo js

---
+++ js/formulario.js
@@ -0,0 +1,22 @@
+let resultado = document.getElementById('resultado');
+let form = document.getElementById('form');
+
+    let usuario = {
+        'firstname': 'Jéssica Gomes',
+        'dataNascimento': '01/03/1995',
+        'email': 'jessika1915@live.com',
+        'complemento': 'casa4',
+    };
+    console.log(usuario)
+
+    let usuarioString = JSON.stringify(usuario);
+    localStorage.setItem('usuario', usuarioString);
+    console.log(localStorage.getItem('usuario'));
+    let dadosArmazenados = localStorage.getItem('usuario');
+    let obj = JSON.parse(dadosArmazenados);
+    console.log(obj);
+
+function limparDados() {
+    localStorage.clear();
+}
+
