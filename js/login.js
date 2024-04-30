d69aaad8fc31111b6d5c5b8a0ae22f537bd1d0e5
Jéssica <163208889+jessicadgomes@users.noreply.github.com>
2024-04-29 21:58:55 -0300

    alterações no login

--- js/login.js
+++ js/login.js
@@ -1,1 +1,22 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const senhaPadrao = 1234
+    const login = 'joao@gmail.com'
+    const notifica = document.getElementById('notifica');
+
+    let senha = document.getElementById('senha').value;
+    let email = document.getElementById('email').value;
+
+    if (senhaPadrao == senha && login == email) {
+        window.open('fila.html', '_self')
+    } else {
+        notifica.innerHTML = 'Email ou senha inválidos!'
+        notifica.open = 'true'
+        setTimeout(() => {
+            notifica.open = false
+            notifica.innerHTML = ''
+        }, 2000);
+    }
+
+});
 
