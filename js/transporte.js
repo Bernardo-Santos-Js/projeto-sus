244b3c239c816002d43911a472ad0ea1ead11400
Bernardo Santos <berd_frontjs@outlook.com>
2024-05-17 21:38:28 -0300

    retirando o botao de noticias para tela maior, e atualizando pagina de transporte

---
+++ js/transporte.js
@@ -0,0 +1,21 @@
+const botao = document.getElementById("botao");
+const navbar = document.getElementById("navbar"); /*const o nome dela é só pra identificar, posso colocar qlqr um*/ /* o toggle ele se  colocar ele aparece o menu sanduiche */
+botao.addEventListener("click", () => {/*classList é a ista de class da navbar*/
+navbar.classList.toggle('menu'); /*vai verificar se a classe ja foi aplicada, se sim ele tira se não ele coloca*/
+    console.log(navbar)
+})
+
+
+
+let contagem = 16;
+
+$('#zoom').click(() => {
+   
+    $('#texto__principal').css('font-size', contagem + "px");
+  if(contagem >= 15){
+    contagem = contagem
+  }else{
+    contagem = contagem + 2;
+  }
+  $('#texto__principal').css('z-index', "-1");
+});
