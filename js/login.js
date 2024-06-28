9b9f1b875eedbbe24fc9df825f7260067ff3c89b
Luan.Santos14 <luan.lsfernandes@senacsp.edu.br>
2024-06-27 21:17:37 -0300

    Link's externos de redirecionamento

--- js/login.js
+++ js/login.js
@@ -1,22 +1,32 @@
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
+    
     const senhaPadrao = 1234
     const login = 'joao@gmail.com'
     const notifica = document.getElementById('notifica');
+
+    
+    const senhaPadra = 123
+    const logi = 'rute@gmail.com'
 
     let senha = document.getElementById('senha').value;
     let email = document.getElementById('email').value;
 
     if (senhaPadrao == senha && login == email) {
         window.open('fila.html', '_self')
-    } else {
-        notifica.innerHTML = 'Email ou senha inválidos!'
-        notifica.open = 'true'
-        setTimeout(() => {
-            notifica.open = false
-            notifica.innerHTML = ''
-        }, 2000);
+    } 
+    else if(senhaPadra == senha && logi == email){
+        window.open('emailConfirmacao.html', '_self')
     }
+    else  {
+        notifica.innerHTML = 'Email ou senha inválidos!'
+        notifica.open = 'false'
+        setTimeout(() => {
+            notifica.open = true
+            notifica.innerHTML = ''
+        }, 200);
+    }
+   
+
 
 });
-
